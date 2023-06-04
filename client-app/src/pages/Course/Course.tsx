baaa00cc378777f5318d3faedc2464fe5e7e46a8
Daniil <gggchaseggg@gmail.com>
2023-06-04 18:54:21 +0300

    create test addon

--- client-app/src/pages/Course/Course.tsx
+++ client-app/src/pages/Course/Course.tsx
@@ -3,8 +3,9 @@ import style from './Course.module.scss'
 import { Link, useNavigate, useParams } from 'react-router-dom'
 import { useCoursesById } from '../../queries/Courses/coursesQueries'
 import PATHS from '../../data/paths'
-import clsx from 'clsx'
 import { UserStore } from '../../mobx'
+import { Box, Button } from '@mui/material'
+import clsx from 'clsx'
 
 const Course = () => {
   const { id } = useParams()
@@ -23,18 +24,39 @@ const Course = () => {
       </div>
       <div className={style.main}>
         <ul className={style.testList}>
-          {course?.tests.map((item) =>
-            <li className={clsx(style.testItem, 'greenCard')} key={item.id}>
-              <Link to={`${PATHS.TESTS}/${item.id}`} key={`test${item.id}${item.theme}`}>
-                {item.theme}
-              </Link>
-            </li>
+          {course?.tests.map((item) => {
+              return UserStore.role === 'teacher'
+                ? <Button>{item.theme}</Button>
+                : (
+                  <li className={clsx(style.testItem, 'greenCard')} key={item.id}>
+                    <Link to={`${PATHS.TESTS}/${item.id}`} key={`test${item.id}${item.theme}`}>
+                      {item.theme}
+                    </Link>
+                  </li>
+                )
+            }
           )}
-          <li className={clsx(style.testItem, 'greenCard')}>
-            <Link to={`${PATHS.TESTS}/new/${id as string}`}>
-              +
-            </Link>
-          </li>
+          {
+            UserStore.role === 'teacher' && (
+              <Box margin={'0 auto'} width={'100%'}>
+                <Link to={`${PATHS.TESTS}/new/${id as string}`}>
+                  <Button
+                    size="large"
+                    variant="outlined"
+                    sx={{
+                      width: '100%',
+                      fontSize: 32,
+                      fontWeight: 600,
+                      marginTop: 3,
+                      border: 'dashed 1px'
+                    }}
+                  >
+                    Добавить тест
+                  </Button>
+                </Link>
+              </Box>
+            )
+          }
         </ul>
       </div>
     </div>
